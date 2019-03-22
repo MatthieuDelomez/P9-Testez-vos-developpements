869a7f7ab10b33831a1060f2b4ae4869a1b83547
markometrie <mdelomez@esac-cambrai.net>
2019-03-22 18:40:48 +0100

    Integration du Pom consumer | Configuration des fichiers de contexte | Classe Test-consumer en dev Prblm sur Transactional

--- src/myerp-consumer/src/test/java/com/dummy/myerp/consumer/test/TestComptabilite.java
+++ src/myerp-consumer/src/test/java/com/dummy/myerp/consumer/test/TestComptabilite.java
@@ -7,18 +7,17 @@ import org.junit.Test;
 
 
 import com.dummy.myerp.consumer.dao.impl.db.dao.ComptabiliteDaoImpl;
-import com.dummy.myerp.model.bean.comptabilite.CompteComptable;
-import com.dummy.myerp.model.bean.comptabilite.EcritureComptable;
-import com.dummy.myerp.model.bean.comptabilite.JournalComptable;
-import com.dummy.myerp.model.bean.comptabilite.LigneEcritureComptable;
-import com.dummy.myerp.technical.exception.FunctionalException;
-import com.dummy.myerp.technical.exception.NotFoundException;
-import java.math.BigDecimal;
-import java.util.Calendar;
-import java.util.GregorianCalendar;
+import org.junit.runner.RunWith;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.transaction.annotation.Transactional;
 
 
-
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations= {"/com/dummy/myerp/consumer/applicationContext.xml"})
+@ActiveProfiles(profiles="test")
+@Transactional
 public class TestComptabilite {
     
     
@@ -27,89 +26,13 @@ public class TestComptabilite {
     */
     ComptabiliteDaoImpl dao = ComptabiliteDaoImpl.getInstance();
     
-    
-    
-    //************************* TEST DE LA METHODE INSERT ECRITURE******************************
-    
-    @Test
-    public void insertEcritureTest() throws FunctionalException, NotFoundException {
-        
-        EcritureComptable vEcritureComptable;
-        vEcritureComptable = new EcritureComptable();
-        vEcritureComptable.setJournal(new JournalComptable("AC", "Achat"));
-        Calendar calendar = new GregorianCalendar(2019, 1, 1);
-        
-        vEcritureComptable.setDate(calendar.getTime());
-        vEcritureComptable.setReference("AC-2019/00001");
-        vEcritureComptable.setLibelle("Libelle");
-        vEcritureComptable.getListLigneEcriture().add(new LigneEcritureComptable(new CompteComptable(401),
-        
-                                                                                                                                                        null, new BigDecimal("341"),
-        
-                                                                                                                                                         null));
-        
-        vEcritureComptable.getListLigneEcriture().add(new LigneEcritureComptable(new CompteComptable(401),
-        
-                                                                                                                                                        null, null, new BigDecimal("341")));
-        
-        dao.insertEcritureComptable(vEcritureComptable);
-        
-        EcritureComptable testEcriture;
-        
-        testEcriture = dao.getEcritureComptableByRef("AC-2019/00001");
-        
-        assertNotNull(testEcriture);
-        
-    }
-    
-    
-    
-    //***************************TEST DE LA METHODE DELETE ECRITURE COMPTABLE************************
-    
-    
-    /*
-    Nous allons tester la méthode pour supprimer une ecriture comptable
-    */
-    /*
-    @Test
-    public void testdeleteEcritureComptableTest() throws NotFoundException {
-        
-        Integer pId = -1 ;
-        EcritureComptable testEcriture;
-        testEcriture = dao.getEcritureComptable(pId);
-        
-        /*
-        Bloc Try qui va vérifier si l'écriture à bien été supprimer
-        
-        try {
-            
-        testEcriture = dao.getEcritureComptable(pId);
-        
-        fail("Exception Not Found");
-        
-        } catch(NotFoundException e) {
-            
-        assert That(e.getMessage(), is("EcritureComptable non trouvée : id =" + pId));
-        
-        }
+                     @Test
+	public void testDaoNull() {
+		assertNotNull(dao);
+
+	}
         
         
-        
-        /*
-        On clean la liste des lignes d'écritures
-        */
-  //      testEcriture.getListLigneEcriture().clear();
-        
-        /*
-        On charge la ligne d'écrite gràâce à la méthode load
-        */
-     //   dao.loadListLigneEcriture(testEcriture);
-        
-        /*
-        Nous vérifions que les lignes d'écritures ont été suprimées
-        */
-        //assertTrue(testEcriture.getListLigneEcriture().isEmpty());
-    
 
     
     
