f159c2f0ca9773038725d96c8660ba0dbf9cdb36
markometrie <mdelomez@esac-cambrai.net>
2019-03-26 14:37:45 +0100

    Methode Insert testee et effective ==> Creation dune nouvelle ligne EcritureComptable via Insert + Test de la delete, Suppression en base fonctionnel via Id de EcritureComptable [Exception attendue lors de la supp]

--- src/myerp-consumer/src/test/java/com/dummy/myerp/consumer/test/TestComptabilite.java
+++ src/myerp-consumer/src/test/java/com/dummy/myerp/consumer/test/TestComptabilite.java
@@ -13,9 +13,15 @@ import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 
 import com.dummy.myerp.consumer.dao.impl.db.dao.ComptabiliteDaoImpl;
+import com.dummy.myerp.model.bean.comptabilite.CompteComptable;
 import com.dummy.myerp.model.bean.comptabilite.EcritureComptable;
 import com.dummy.myerp.model.bean.comptabilite.JournalComptable;
+import com.dummy.myerp.model.bean.comptabilite.LigneEcritureComptable;
+import com.dummy.myerp.technical.exception.FunctionalException;
 import com.dummy.myerp.technical.exception.NotFoundException;
+import java.math.BigDecimal;
+import java.util.Calendar;
+import java.util.GregorianCalendar;
 
 
 
@@ -30,8 +36,10 @@ import com.dummy.myerp.technical.exception.NotFoundException;
                                             Implementation de la classe ComptaDao que nous allons tester
                                             */
                                             ComptabiliteDaoImpl dao = ComptabiliteDaoImpl.getInstance();
+                                            
     
-        
+                                            //************************************************************************************************************/
+                                            
                                             /*
 	                      * Vérification de l'obtention de la liste des écritures comptables
                                             */
@@ -116,13 +124,114 @@ import com.dummy.myerp.technical.exception.NotFoundException;
                                             */
                                             
                                             // La taille de la liste est bien égale à 4
-                                            assertTrue("Nous allons tester la liste attendu, si elle correspond bien à la taille donnée", listeJournaux.size() ==  4);
+                                            assertTrue("Nous allons tester la liste attendu, si elle correspond bien à la taille donnée", listeJournaux.size() ==  4 );
                                             
                                             // Nom du code du journal = AC | Libelle = Achat
                                             assertTrue("Test si le journal est bien existant", listeJournaux.stream().filter(o -> o.getCode().equals("AC")).findFirst().isPresent());
 		
 		
 	                      }
+                                            
+                                            
+                                            //************************************************************************************************************/
+                                            
+                                            /*
+                                            Création de la methode qui va tester la requête Sql InsertEcriture 
+                                           
+                                            @Test
+                                            public void insertSqlEcritureTest() throws FunctionalException, NotFoundException{
+                                                
+                                                
+                                            EcritureComptable ecritureComptable;
+                                            
+                                            //Création d'une variable écriture comptable
+                                            ecritureComptable = new EcritureComptable();
+                                            
+                                            //Nous allons Set écriture comptable au nom de code du journal comptable ainsi qu'a son libelle
+                                            ecritureComptable.setJournal(new JournalComptable("AC", "Achat"));
+                                            
+                                            //Création d'une variable Calendar qui sera défini via un GregorianCalendar
+                                            Calendar calendar = new GregorianCalendar(2019,1, 1);
+                                            
+                                            //Nous allons Set la date sur l'écriture comptable
+                                            ecritureComptable.setDate(calendar.getTime());
+                                            
+                                            //Nous définissons la référence de l'écriture comptable
+                                            ecritureComptable.setReference("AC-2019/00001");
+                                            
+                                            //On set le libelle
+                                            ecritureComptable.setLibelle("Libelle");
+                                            
+                                            // 401 = Fournisseur | 512 = Banque
+                                            ecritureComptable.getListLigneEcriture().add(new LigneEcritureComptable(new CompteComptable(401), null, new BigDecimal("35"), null));
+                                            
+                                            ecritureComptable.getListLigneEcriture().add(new LigneEcritureComptable(new CompteComptable(512), null, null, new BigDecimal("35")));
+
+                                            //On initialise la requête dans la variable écriture comptable
+                                            dao.insertEcritureComptable(ecritureComptable);
+                                            
+                                            //Initialisation d'une variable test
+                                            EcritureComptable testEcritureComptable;
+                                            
+                                            //Définition de la variable test en fonction de sa référence
+                                            testEcritureComptable = dao.getEcritureComptableByRef("AC-2019/00001");
+                                            
+                                            //Nous vérifions que la variable TestEcriture n'est pas null
+                                                assertNotNull(testEcritureComptable);
+                                            
+                                            }*/
+                                            
+                                            
+                                            //************************************************************************************************************/
+                                            
+                                            
+                                            /*
+                                            Création de la méthode test pour supprimer une écriture comptable en base de donnée
+                                            @throws NotFoundException
+                                            */
+                                            @Test
+                                            public void deleteEcritureComptableTest() throws NotFoundException {
+                                            
+                                            // Référence de l'écriture
+                                            Integer Id = -1;
+                                            
+                                            EcritureComptable testEcriture;
+                                            
+                                            testEcriture = dao.getEcritureComptable(Id);
+                                            
+                                            //Invocation de la méthode delete
+                                            dao.deleteEcritureComptable(Id);
+                                            
+                                            
+                                            
+                                            
+                                            //Vérification que l'écriture à bien été supprimée [Exception attendu]
+                                            
+                                            try {
+                                                
+                                            testEcriture = dao.getEcritureComptable(Id);
+                                            
+                                            fail("Exception attendue via NotFoundException");
+                                            
+                                            } catch (NotFoundException exception) {
+                                                
+                                            assertThat(exception.getMessage(), is("L'écriture comptale n'a pas été trouvée ! [Normal elle vient d'être supp] Id = " + Id));
+                                            
+                                            }
+                                            
+                                           // On nettoie la liste des lignes d'écritures
+                                           testEcriture.getListLigneEcriture().clear();
+                                           
+                                           //On charge la liste des lignes d'écritures via la méthode Load du Dao
+                                           dao.loadListLigneEcriture(testEcriture);
+                                           
+                                           
+                                           
+                                            //Vérification que les lignes ont bien étaient supprimées
+                                            assertTrue(testEcriture.getListLigneEcriture().isEmpty());
+
+                                                
+                                            }
 	
                                             
                                             }
