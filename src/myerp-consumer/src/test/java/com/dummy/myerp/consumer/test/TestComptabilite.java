6f6e0d7ef8c7e8f287759b83cdc9aa4d64dc52df
markometrie <mdelomez@esac-cambrai.net>
2019-03-26 12:31:04 +0100

    Debut des implementations Test des methodes du module Consumer | Verification de la taille de la liste Ecritures Comptable ainsi que la presence dune ecriture ==> getListEcritureComptable

--- src/myerp-consumer/src/test/java/com/dummy/myerp/consumer/test/TestComptabilite.java
+++ src/myerp-consumer/src/test/java/com/dummy/myerp/consumer/test/TestComptabilite.java
@@ -1,36 +1,47 @@
 
 package com.dummy.myerp.consumer.test;
 
-import static org.hamcrest.CoreMatchers.is;
 import static org.junit.Assert.*;
 import org.junit.Test;
 
 
 import com.dummy.myerp.consumer.dao.impl.db.dao.ComptabiliteDaoImpl;
+import com.dummy.myerp.model.bean.comptabilite.EcritureComptable;
+import java.util.List;
 import org.junit.runner.RunWith;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
-import org.springframework.transaction.annotation.Transactional;
 
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration(locations= {"/com/dummy/myerp/consumer/applicationContext.xml"})
 @ActiveProfiles(profiles="test")
-@Transactional
 public class TestComptabilite {
     
     
-    /*
-    Implementation de la classe ComptaDao que nous allons tester
-    */
-    ComptabiliteDaoImpl dao = ComptabiliteDaoImpl.getInstance();
+                       /*
+                      Implementation de la classe ComptaDao que nous allons tester
+                      */
+                       ComptabiliteDaoImpl dao = ComptabiliteDaoImpl.getInstance();
     
-                     @Test
-	public void testDaoNull() {
-		assertNotNull(dao);
+        
+                      /*
+	* Vérification de l'obtention de la liste des écritures comptables
+                      */
+	@Test
+	public void getListEcritureComptableTest() {
+            
+		List<EcritureComptable>liste;
+		
+		liste=dao.getListEcritureComptable();
 
+		assertTrue("Test taille de la liste attendu Ecritures Comptables", liste.size() == 5);
+		assertTrue("Test si une ecriture test est bien présente",liste.stream().filter(o -> o.getReference().equals("BQ-2016/00005")).findFirst().isPresent());
+		
+		
 	}
+        
         
         
 
